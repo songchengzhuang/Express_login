53c6b8c82fbd0f9040cd9c16735dd079c00f8b5f
songcz <songcz@ufotosoft.com>
2018-09-10 10:29:03 +0800

    跨域

--- app.js
+++ app.js
@@ -35,11 +35,11 @@ app.use('/dealData', usersRouter);
 
 // 跨域请求
 app.all('*', (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080"); //为了跨域保持session，所以指定地址，不能用*
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By", ' 3.2.1')
-  res.header("Content-Type", "application/json;charset=utf-8");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 
